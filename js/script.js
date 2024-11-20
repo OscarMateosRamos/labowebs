8e766066c1fadd9bd36afedf3616c1d5efda6ac0
Osmara <dsi85944@educastur.es>
2024-11-20 22:07:36 +0100

    Revert "Cambios realizados en script.js"
    This reverts commit ffc216c0eeb835b8079ae8bcd03f1fa6a4bfe1de.

--- js/script.js
+++ js/script.js
@@ -1,29 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("formContacto");
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("formContacto");
+    
+    form.addEventListener("submit", function(event) {
+        let valid = true;
 
-  form.addEventListener("submit", function (event) {
-    let valid = true;
+        const nombre = document.getElementById("nombre");
+        const email = document.getElementById("email");
+        const mensaje = document.getElementById("mensaje");
 
-    const nombre = document.getElementById("nombre");
-    const email = document.getElementById("email");
-    const mensaje = document.getElementById("mensaje");
+        if (nombre.value.trim() === "") {
+            alert("Por favor, ingrese su nombre.");
+            valid = false;
+        }
 
-    if (nombre.value.trim() === "") {
-      alert("Por favor, ingrese su nombre.");
-      valid = false;
-    }
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) {
+            alert("Por favor, ingrese un correo electrónico válido.");
+            valid = false;
+        }
 
-    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) {
-      alert("Por favor, ingrese un correo electrónico válido.");
-      valid = false;
-    }
-
-    if (!valid) {
-      event.preventDefault();
-    }
-    if (mensaje.value.trim() === "") {
-      alert("Ingrese un mensaje, por favor");
-      valid = false;
-    }
-  });
+        
+        if (!valid) {
+            event.preventDefault();
+        }
+    });
 });
