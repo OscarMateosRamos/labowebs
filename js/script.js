ffc216c0eeb835b8079ae8bcd03f1fa6a4bfe1de
Osmara <dsi85944@educastur.es>
2024-11-20 22:07:05 +0100

    Cambios realizados en script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("formContacto");
-    
-    form.addEventListener("submit", function(event) {
-        let valid = true;
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("formContacto");
 
-        const nombre = document.getElementById("nombre");
-        const email = document.getElementById("email");
-        const mensaje = document.getElementById("mensaje");
+  form.addEventListener("submit", function (event) {
+    let valid = true;
 
-        if (nombre.value.trim() === "") {
-            alert("Por favor, ingrese su nombre.");
-            valid = false;
-        }
+    const nombre = document.getElementById("nombre");
+    const email = document.getElementById("email");
+    const mensaje = document.getElementById("mensaje");
 
-        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) {
-            alert("Por favor, ingrese un correo electrónico válido.");
-            valid = false;
-        }
+    if (nombre.value.trim() === "") {
+      alert("Por favor, ingrese su nombre.");
+      valid = false;
+    }
 
-        
-        if (!valid) {
-            event.preventDefault();
-        }
-    });
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) {
+      alert("Por favor, ingrese un correo electrónico válido.");
+      valid = false;
+    }
+
+    if (!valid) {
+      event.preventDefault();
+    }
+    if (mensaje.value.trim() === "") {
+      alert("Ingrese un mensaje, por favor");
+      valid = false;
+    }
+  });
 });
